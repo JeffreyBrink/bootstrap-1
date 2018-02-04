f505b1d6a89dc89d19ae44319c5ef921fecd9509
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-04 15:26:50 -0700

    added jumbotron

--- index.php
+++ index.php
@@ -1,7 +1,6 @@
 <!doctype html>
 <html lang="en">
 	<head>
-		<!-- Required meta tags -->
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
@@ -9,58 +8,56 @@
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
 		<title>Hello, world!</title>
+
 	</head>
 	<body>
-
 		<div class="container">
-			<div class="row">
-				<div class="col-sm-4">.col-sm-4</div>
-				<div class="col-sm-4">.col-sm-4</div>
-				<div class="col-sm-4">.col-sm-4</div>
+			<div class="row align-items-start">
+
+				<div class="col">
+					<div class="jumbotron">
+						<h1 class="display-3">Hello, world!</h1>
+						<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+						<hr class="my-4">
+						<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+						<p class="lead">
+							<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+						</p>
+					</div>
+					One of three columns
+				</div>
+				<div class="col">
+					One of three columns
+				</div>
+				<div class="col">
+					One of three columns
+				</div>
 			</div>
-			<div class="jumbotron">
-					<nav class="navbar fixed-top navbar-light bg-light">
-						<a class="navbar-brand" href="#">Fixed top</a>
-					</nav>
-
-
-					<nav class="navbar navbar-expand-lg navbar-light bg-light">
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-							<span class="navbar-toggler-icon">
-							</span>
-						</button>
-						<a class="navbar-brand" href="#">Navbar</a>
-
-						<div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-							<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-								<li class="nav-item active">
-									<a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link" href="#">Link</a>
-								</li>
-								<li class="nav-item">
-									<a class="nav-link disabled" href="#">Disabled</a>
-								</li>
-							</ul>
-							<form class="form-inline my-2 my-lg-0">
-								<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-								<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-							</form>
-						</div>
-					</nav>
-					<div class="w-22 p-3"</div>
-				<p class="text-left"></p>
-				<h1>Hello world!</h1>
-				<p class="text-center"></p>
-				<p class="lead">
-				<p>This is a hero unit</p>
-				<p>bla bla bla</p>
-				<p class="lead">
-					<a class="btn-primary btn-lg" href="#" role="button">learn more</a>
-				</p>
+			<div class="row align-items-center">
+				<div class="col">
+					One of three columns
+				</div>
+				<div class="col">
+					One of three columns
+				</div>
+				<div class="col">
+					One of three columns
+				</div>
+			</div>
+			<div class="row align-items-end">
+				<div class="col">
+					One of three columns
+				</div>
+				<div class="col">
+					One of three columns
+				</div>
+				<div class="col">
+					One of three columns
+				</div>
 			</div>
 		</div>
+
+
 
 	</body>
 </html>
