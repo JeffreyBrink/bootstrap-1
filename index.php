d22e9173b8640b19d9cddc26eeb8e1736d04a6cf
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-04 17:13:49 -0700

    added page 2

--- index.php
+++ index.php
@@ -21,8 +21,6 @@
 					</nav>
 
 					<div class="jumbotron">
-
-
 						<h1 class="display-3">Hello, world!</h1>
 						<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 
@@ -31,36 +29,56 @@
 							<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 						</p>
 					</div>
-					One of three columns
+
+					One of three columns1
+				</div>
+
+					<div class="col">
+						<nav class="navbar navbar-light bg-light justify-content-between">
+							<a class="navbar-brand">Title</a>
+
+						</nav>
+					<div class="jumbotron">
+						<h1 class="display-3">Hello, world!</h1>
+						<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+
+						<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+						<p class="lead">
+							<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+						</p>
+					</div>
+					One of three columns2
 				</div>
 				<div class="col">
-					One of three columns
-				</div>
-				<div class="col">
-					One of three columns
+					One of three columns3
 				</div>
 			</div>
 			<div class="row align-items-center">
 
 				<div class="col">
-					One of three columns
+
+					One of three columns4
 				</div>
 				<div class="col">
-					One of three columns
+
+
+					One of three columns5
 				</div>
 				<div class="col">
-					One of three columns
+					One of three columns6
 				</div>
 			</div>
 			<div class="row align-items-end">
 				<div class="col">
-					One of three columns
+					One of three columns7
 				</div>
 				<div class="col">
-					One of three columns
+
+
+					One of three columns8
 				</div>
 				<div class="col">
-					One of three columns
+					One of three columns9
 				</div>
 			</div>
 		</div>
