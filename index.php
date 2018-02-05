82700e644798dfe24970f62a12ea5824cab3d6c4
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-04 17:32:47 -0700

    added second image

--- index.php
+++ index.php
@@ -39,12 +39,13 @@
 
 						</nav>
 					<div class="jumbotron">
-						<h1 class="display-3">Hello, world!</h1>
+
+						<img src="stress.jpg" class="img-rounded" alt="stressed jeff">
 						<p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
 
 						<p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
 						<p class="lead">
-							<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+
 						</p>
 					</div>
 					One of three columns2
