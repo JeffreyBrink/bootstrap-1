effc60c278f72a1fff93855bc4169cd88da5f3fd
Jeffrey Brink <JeffreyBrink@GMX.com>
2018-02-15 13:09:12 -0700

    added pwp content

--- pwp.php
+++ pwp.php
@@ -2,7 +2,6 @@
 <html>
 	<head>
 		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<link rel="stylesheet" type="text/css" href="stylesheet.css">
 		<style>
 			* {
 				box-sizing: border-box;
@@ -31,30 +30,48 @@
 				padding: 20px;
 			}
 
-
+			#myBtn {
+				width: 200px;
+				font-size: 18px;
+				padding: 10px;
+				border: none;
+				background: #000;
+				color: #fff;
+				cursor: pointer;
 			}
 
-
+			#myBtn:hover {
+				background: #ddd;
+				color: black;
 			}
 		</style>
 	</head>
 	<body>
 
 		<video autoplay muted loop id="myVideo">
-			<source src="nebula.mp4" type="video/mp4">
+			<source src="pwp-video.mp4" type="video/mp4">
 			Your browser does not support HTML5 video.
 		</video>
 
-		<div>class="content"
-		<div>class="right"
+
+		<div class="content">
 			<h1>Heading</h1>
 			<p>Lorem ipsum dolor sit amet, an his etiam torquatos. Tollit soleat phaedrum te duo, eum cu recteque expetendis neglegentur. Cu mentitum maiestatis persequeris pro, pri ponderum tractatos ei. Id qui nemore latine molestiae, ad mutat oblique delicatissimi pro.</p>
+			<button id="myBtn" onclick="myFunction()">Pause</button>
 		</div>
-		</div>
-
 
 		<script>
 			var video = document.getElementById("myVideo");
+			var btn = document.getElementById("myBtn");
+
+			function myFunction() {
+				if (video.paused) {
+					video.play();
+					btn.innerHTML = "Pause";
+				} else {
+					video.pause();
+					btn.innerHTML = "Play";
+				}
 			}
 		</script>
 
